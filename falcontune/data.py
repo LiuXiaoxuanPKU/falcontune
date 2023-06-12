71ca563d27c497675c6f8b2f60cdc91258a0f6d8
LiuXiaoxuanPKU <lilyliupku@gmail.com>
2023-06-12 23:50:13 +0000

    first commit

--- falcontune/data.py
+++ falcontune/data.py
@@ -168,24 +168,82 @@ class TrainSAD(TrainDataBase):
         return make_prompt(
             data_point["instruction"],
             data_point["input"],
-            data_point["output"]
+            data_point["output"],
+            type="sad"
         )
 
     def generate_and_tokenize_prompt(self, data_point, **kwargs):
         prompt = self.generate_prompt(data_point, **kwargs)
         return self.tokenize(prompt, **kwargs)
 
+class TrainShareGPT(TrainDataBase):
+    def __init__(self, dataset: str, val_set_size: int, tokenizer, cutoff_len) -> None:
+        super().__init__(dataset, val_set_size, tokenizer, cutoff_len)
+    
+    def prepare_data(self, **kwargs) -> None:
+        data = load_dataset("json", data_files=self.dataset)
+        
+        self.val_data = None
+        if self.val_set_size > 0:
+            pass
+        else:
+            self.train_data = data["train"].shuffle().map(lambda x: self.generate_and_tokenize_prompt(x))
+            self.val_data = None
 
-def make_prompt(instruction, input_, output=""):
-    return "{0}\n\n{1}\n{2}\n\n{3}\n{4}\n\n{5}\n{6}".format(
-        "Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.",
-        "### Instruction:",
-        instruction,
-        "### Input:",
-        input_,
-        "### Response:",
-        output
-    )
+        train_dataset = train_dataset.map(
+            lambda ele: self.tokenize_inputs(ele),
+            batched=True,
+            remove_columns=["source", "prompt"],
+        )
+        self.train_data = train_dataset.with_format("torch")
+
+    def generate_prompt(self, data_point):
+        return make_prompt(data_point["conversations"])
+    
+    def generate_and_tokenize_prompt(self, data_point):
+        prompt = self.generate_prompt(data_point)
+        label = torch.tensor(tokenized_full_prompt["input_ids"])
+        if prompt == "IGNORE":
+            pass
+        tokenized_full_prompt = self.tokenize(prompt)
+        
+        
+    
+def make_prompt(instruction, input_, output="", conversation=[], type="shareGPT"):
+    if type == "shareGPT":
+        # returns the full prompt
+        if len(conversation) > 0 and conversation[0]['from'] != 'human':
+                conversation = conversation[1:]
+
+        if len(conversation) > 0 and conversation[0]['from'] != 'human':
+            return "IGNORE"
+
+        if len(conversation) == 0:
+            return "IGNORE"
+
+        joined_conversation = ""
+        for i, e in enumerate(conversation):
+            if i % 2 == 0:
+                if e['from'] != 'human':
+                    return "IGNORE"
+                joined_conversation += f"USER: {e['value']} "
+            else:
+                if e['from'] != 'gpt':
+                    return "IGNORE"
+                joined_conversation += f"ASSISTANT: {e['value']}</s>"
+
+        instruction = "A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions. "
+        return f"{instruction}{joined_conversation}"
+    else:
+        return "{0}\n\n{1}\n{2}\n\n{3}\n{4}\n\n{5}\n{6}".format(
+            "Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.",
+            "### Instruction:",
+            instruction,
+            "### Input:",
+            input_,
+            "### Response:",
+            output
+        )
 
 
 def load_data(config, tokenizer):
@@ -202,7 +260,13 @@ def load_data(config, tokenizer):
             config.val_set_size,
             tokenizer,
             config.cutoff_len)
-
+    elif config.data_type == "shareGPT":
+        data = TrainShareGPT(
+            config.dataset,
+            config.val_set_size,
+            tokenizer,
+            config.cutoff_len
+        )
     else:
         raise ValueError(f"Invalid data name: {config.data_type}")
 
@@ -213,4 +277,5 @@ def load_data(config, tokenizer):
 DATA_TYPES = [
     "alpaca",
     "gpt4all",
+    "shareGPT"
 ]
